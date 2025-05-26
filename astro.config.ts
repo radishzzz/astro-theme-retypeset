2c1c706881fe5ce0b9c23ad97062c5431195968c
radishzzz <shuyinsen@gmail.com>
2025-05-26 15:16:48 +0100

    test: enable astro-compress

--- astro.config.ts
+++ astro.config.ts
@@ -60,10 +60,10 @@ export default defineConfig({
     sitemap(),
     robotsTxt(),
     Compress({
-      CSS: false,
-      HTML: false,
+      CSS: true,
+      HTML: true,
       Image: false,
-      JavaScript: false,
+      JavaScript: true,
       SVG: false,
     }),
   ],
